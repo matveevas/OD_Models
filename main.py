513e1ba2a2730b35ec31cb42195f0d77eb4a3e7c
svetlana.matveeva <matveevas94@yandex.ru>
2018-07-03 00:06:23 +0300

    outlier detection models

--- main.py
+++ main.py
@@ -30,13 +30,15 @@ spark = SparkSession\
     .config("spark.some.config.option", "some-value")\
     .getOrCreate()
 
-df =spark.read.csv("/Users/svetlana.matveeva/Documents/MasterThesis/Dataset/joinresult/part-00000-647e9d46-6169-41d6-8dab-948c865be136-c000.csv")
-df.printSchema()
-df.select("_c1").show()
-df.select(regexp_replace("_c1","POINT (", "").alias("point")).collect()
-# df.withColumn('_c1', regexp_replace(df("_c1"),"POINT (", ""))
-#df.withColumn('address', regexp_replace('address', 'lane', 'ln'))
-df.select("_c1").show()
+df3 = spark.read.csv("/Users/svetlana.matveeva/Documents/MasterThesis/Dataset/joinresult/part-00000-056da02e-ec76-43d8-873f-10a6a8ee8029-c000.csv")
+df3.printSchema()
+df3.select("_c2").show()
+df1 = df3.select("_c1", regexp_replace("_c0", "POLYGON [(][(]", "").alias("polygon"), regexp_replace("_c2", "POINT [(]", "").alias("point"), "_c3", "_c4", "_c5")
+df2 = df1.select("_c1", regexp_replace("polygon", "[)][)]", "").alias("polygon"), regexp_replace("point", "[)]", "").alias("point"), "_c3", "_c4", "_c5")
+df = df2.withColumnRenamed("_c1", "polygonID").withColumnRenamed("_c3", "pointID").withColumnRenamed("_c4", "addresstext").withColumnRenamed("_c5", "creteddatetime")
+df.createTempView("df")
+# dfCNT = spark.sql("select polygonID, creteddatetime, count(pointID) as count from df groupby polygonID, cretaeddatetime")
+# "polygonID", "creteddatetime"
 
 
 print("here")
